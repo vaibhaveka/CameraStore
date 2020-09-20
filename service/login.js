60676b4f6fb2463f493dc8a2c71737e55330504e
vaibhaveka <vaibhav.singh@ekaplus.com>
2020-09-20 12:20:38 +0530

    name change

--- service/login.js
+++ service/login.js
@@ -1,8 +1,8 @@
 (function(){
-    var login;
-    login = function(){};
+    var loginService;
+    loginService = function(){};
     const User = require('../models/users');
-    login.register = async function(req,res){
+    loginService.register = async function(req,res){
         let body=req.body;
         try{
             let email = body.email;
@@ -17,16 +17,23 @@
             res.status(400).send({ error: err });
         }
     },
-    login.login = async function(req,res){
+    loginService.login = async function(req,res){
         let body=req.body;
         try{
           let email = body.email;
           let password  = body.password;
-          const user =  await User.find({$and:[{email: email},{password:password}]});
-          res.status(200).send({data:user[0],msg:"successful"});
+          const user =  await User.findOne({email: email});
+            if(user){
+                if(user.password==password)
+                    res.status(200).send({data:user,status:"successful"});
+                else
+                    res.status(400).send({msg:"wrong password",status:"failed"});
+            }else{
+                res.status(400).send({msg:"User not registered",status:"failed"});
+            }
         }catch(err) {
           res.status(400).send({ error: err });
         }
       }
-module.exports = login;
+module.exports = loginService;
 }.call(this))
